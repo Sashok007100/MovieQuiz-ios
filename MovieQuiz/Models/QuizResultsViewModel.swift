ad7eb163deb76a6123a155e846a582c71b672470
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2023-09-09 21:14:13 +0300

    Были добавлены комментарии типа // MARK: - ... . Модели были вынесены из MovieQuizViewController в отдельные файлы, которые теперь хранятся в отдельной папке Models.

---
+++ MovieQuiz/Models/QuizResultsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  QuizResultsViewModel.swift
+//  MovieQuiz
+//
+//  Created by Alexandr Artemov (Mac Mini) on 09.09.2023.
+//
+
+import Foundation
---
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+//
+//  Created by Alexandr Artemov (Mac Mini) on 09.09.2023.
+//
+
+import Foundation
