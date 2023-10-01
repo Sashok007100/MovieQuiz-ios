d9aecfa69767ad805da43672cc7f62da539bc23e
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2023-10-01 14:20:47 +0300

    Написан класс StatisticServiceImplementation для ведения статистики, который будет учитывать лучший счёт игры в квиз среди всех игр (даже между перезапусками).

--- MovieQuiz/Services/StatisticService.swift
+++ MovieQuiz/Services/StatisticService.swift
@@ -1,8 +1,9 @@
-//
-//  StatisticService.swift
-//  MovieQuiz
-//
-//  Created by Alexandr Artemov (Mac Mini) on 30.09.2023.
-//
-
 import Foundation
+
+protocol StatisticService {
+    var totalAccuracy: Double { get }
+    var gamesCount: Int { get }
+    var bestGame: GameRecord? { get }
+    
+    func store(correct count: Int, total amount: Int)
+}
--- MovieQuiz/Services/StatisticServiceImplementation.swift
+++ MovieQuiz/Services/StatisticServiceImplementation.swift
@@ -1,8 +1,78 @@
-//
-//  StatisticServiceImplementation.swift
-//  MovieQuiz
-//
-//  Created by Alexandr Artemov (Mac Mini) on 30.09.2023.
-//
-
 import Foundation
+
+final class StatisticServiceImplementation: StatisticService {
+    
+    // MARK: - Private Enum
+    private enum Keys: String {
+        case correct, total, bestGame, gamesCount
+    }
+    
+    // MARK: - Private Properties
+    private let userDefaults = UserDefaults.standard
+    
+    // MARK: - Public Properties
+    var totalAccuracy: Double {
+        Double(correct) / Double(total) * 100
+    }
+    
+    var gamesCount: Int {
+        get {
+            userDefaults.integer(forKey: Keys.gamesCount.rawValue)
+        }
+        set {
+            userDefaults.set(newValue, forKey: Keys.gamesCount.rawValue)
+        }
+    }
+    
+    var bestGame: GameRecord? {
+        get {
+            guard let data = userDefaults.data(forKey: Keys.bestGame.rawValue),
+                  let record = try? JSONDecoder().decode(GameRecord.self, from: data) else {
+                return .init(correct: 0, total: 0, date: Date())
+            }
+            return record
+        }
+        set {
+            guard let data = try? JSONEncoder().encode(newValue) else {
+                print("Невозможно сохранить результат")
+                return
+            }
+            userDefaults.set(data, forKey: Keys.bestGame.rawValue)
+        }
+    }
+    
+    var total: Int {
+        get {
+            userDefaults.integer(forKey: Keys.total.rawValue)
+        }
+        set {
+            userDefaults.set(newValue, forKey: Keys.total.rawValue)
+        }
+    }
+    
+    var correct: Int {
+        get {
+            userDefaults.integer(forKey: Keys.correct.rawValue)
+        }
+        set {
+            userDefaults.set(newValue, forKey: Keys.correct.rawValue)
+        }
+    }
+    
+    // MARK: - Public Methods
+    func store(correct count: Int, total amount: Int) {
+        self.correct += count
+        self.total += amount
+        self.gamesCount += 1
+        
+        let currentBestGame = GameRecord(correct: count, total: amount, date: Date())
+        
+        if let previousBestGame = bestGame {
+            if currentBestGame.isBetterThan(previousBestGame) {
+                bestGame = currentBestGame
+            }
+        } else {
+            bestGame = currentBestGame
+        }
+    }
+}
